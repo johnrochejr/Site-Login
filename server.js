cda92a21c94a64c94f9ba273bfb34073f2466cbe
John Roche <roche.johnjr@gmail.com>
2017-08-01 09:02:38 -0400

    added authentication i think

--- server.js
+++ server.js
@@ -12,23 +12,40 @@ app.engine('mst', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mst');
 
-let users = {
-  users: [
-    { id: 0, userName: 'user0', passWord: 'password0' },
-    { id: 1, userName: 'user1', passWord: 'password1'}
-  ]
-}
+// let users = {
+//   users: [
+//     { id: 0, userName: 'user0', passWord: 'password0' },
+//     { id: 1, userName: 'user1', passWord: 'password1' }
+//   ]
+// }
+
+// Process:
+// Client requests secure resource
+// Server requests username and password
+// Client sends username and password
+// Server returns requested resource
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+
+const authenticate = (req, res, next) => {
+
+  if (req.query.userName === "username" && req.query.passWord === "password") {
+    next();
+  } else {
+    res.redirect('login')
+  }
+}
 
 app.get('/login', (req, res) => {
   // res.render('home');
   res.render('login');
 });
 
-app.post('/login', (req, res) => {
-  // res.render('login');
+app.use(authenticate);
+app.get('/', (req, res) => {
+  res.render('home');
 })
 
 app.listen(3000, () => {
